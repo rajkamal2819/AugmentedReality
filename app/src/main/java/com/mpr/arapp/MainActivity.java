6da442c309d38df9063c117fdf77ac25890fcc92
Rajkamal <rajkamal.singh1011@gmail.com>
2022-03-12 20:13:42 +0530

    Initial commit

--- app/src/main/java/com/mpr/arapp/MainActivity.java
+++ app/src/main/java/com/mpr/arapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //LOL THIS IS JEET
+        // ujsdidf kjkf
     }
 }
